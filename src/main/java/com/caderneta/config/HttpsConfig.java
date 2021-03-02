7697984b14f11d806a7ed77f46bf93b851135f0b
siron azevedo <sirondba@gmail.com>
2021-03-02 11:57:01 -0300

    Heroku config

--- src/main/java/com/caderneta/config/HttpsConfig.java
+++ src/main/java/com/caderneta/config/HttpsConfig.java
@@ -2,11 +2,13 @@ package com.caderneta.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.createam.heroku.https.HttpsEnforcer;
 
 @Configuration
+@ComponentScan(basePackages = "com.caderneta.config")
 public class HttpsConfig {
 	
 	@Bean
